2f703d06e931e85347db05c7331507c6cffe6a84
Niek Naber <niek_naber@hotmail.com>
2018-10-11 11:23:37 +0200

    change 41, change gitignore

--- week5/firstAttempt/41/main.cc
+++ week5/firstAttempt/41/main.cc
@@ -1,14 +1,24 @@
 #include "main.ih"
 
-int main(int argc, char *argv[])
+extern char **environ;
+
+int main(int argc, char **argv)
 {
-    // print all strings in argv, by index untill argc
-    for (int index = 0; index < argc; ++index)
-        cout << argv[argc] <<'\n';
+    // swap pointers
+    char **tmp = argv;
+    argv = environ;
+    environ = tmp;
 
     size_t index = 0;
-
-    // print all environment variables until null is found in array
+    // print all ntbs in argv
+    while(argv[index] != 0)
+    {
+        cout << argv[index] << '\n';
+        ++index;
+    }  
+    
+    index = 0;
+    // print all ntbs in environ
     while(environ[index] != 0)
     {
         cout << environ[index] << '\n';
