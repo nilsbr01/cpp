685ba33ff6f735478afddbd0aaed41ab0a64be91
Nils <nilsbrouwer@protonmail.com>
2018-10-24 14:40:12 +0200

    opdracht 54

---
+++ week6/firstattempt/54/sort/sort.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDED_SORT_
+#define INCLUDED_SORT_
+
+#include <iostream>
+#include <string>
+
+class Sort
+{
+    public:
+        // Sort(int (*function)(string **string1, string **string2));
+        Sort();
+        void sort(std::string *array, size_t elements);
+
+    private:
+};
+        
+#endif
