5b76a9a3dea90b0ed2429cd05d9e6be593daeb5b
Hidde Folkertsma <h.folkertsma.1@student.rug.nl>
2018-12-24 20:36:47 +0100

    37, ternary may be not allowed

---
+++ 2/week5/37/37.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main()
+{
+    map<string, size_t> words;
+    string word;
+
+    while (cin >> word)
+    	words[word] = words.count(word) > 0 ? ++words[word] : 1;
+
+    for (const auto &entry : words)
+    	cout << entry.first << ' ' << entry.second << '\n';
+}
