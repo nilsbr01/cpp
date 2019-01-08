28472c2fd8a3bfd0e1a9d692a8be833183a35a9c
Hidde Folkertsma <h.folkertsma.1@student.rug.nl>
2019-01-08 12:42:40 +0100

    use equal_range to simplify 41

--- 2/week5/41/main.cc
+++ 2/week5/41/main.cc
@@ -14,19 +14,10 @@ int main()
         {"3","4"}
     };
     
-    size_t nElements = map.size();    // total number of elements
-    size_t nDuplicates = 0;           // initialize number of duplicates
-    
-    for (auto iter = map.begin(); iter != map.end(); ++iter)
-    {
-        // counts occurence of key
-        size_t keyDuplicates = map.count(iter->first);
-        // add duplicats minus one (since this is unique)
-        nDuplicates += keyDuplicates - 1;
-        // move to next item
-        advance(iter, keyDuplicates - 1);
-    }    
-    
-    size_t nUniqueKeys = nElements - nDuplicates;
+    size_t nUniqueKeys = 0;
+
+    for (auto iter = map.begin(); iter != map.end(); iter = map.equal_range(iter->first).second)
+        ++nUniqueKeys;
+
     cout << "There are " << nUniqueKeys << " in the container\n";
 }
